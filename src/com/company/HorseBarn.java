b33a6664eb662a29863ec70926cb2c91e041adb7
BT_1N3_33@bktech.local <BT_1N3_33@bktech.local>
2018-11-19 08:41:48 -0500

    Initial commit

---
+++ src/com/company/HorseBarn.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class HorseBarn {
+    public int findHorseSpace(String name) {
+        for (int i = 0; i < this.spaces.length; i++) {
+            if (this.spaces[i]!=null && name.equals(this.space[i].getName())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void consolidate() {
+        for (int i= 0; i < this.spaces.length-1; i++) {
+            if (this.spaces[i] == null) {
+                for (int j = i+1; j < this.spaces.length; j++) {
+                    if (this.spaces[j] !=null) {
+                        this.spaces[i] = this.spaces[j];
+                        this.spaces[j] = null;
+                        j = this.spaces.length;
+                    }
+                }
+            }
+        }
+    }
+}
