dcbc370f2a1f292a5af016b7f77a0a69cf6faa94
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2018-11-19 13:48:03 -0500

    Update Mustang.java

--- src/com/company/Mustang.java
+++ src/com/company/Mustang.java
@@ -1,5 +1,21 @@
 package com.company;
 
-public class Mustang {
-
+public class Mustang implements Horse {
+  private String name;
+  private int weight;
+  public Mustang(String name, int weight) {
+    this.name = name;
+    this.weight = weight;
+  }
+  
+  public String getName() {
+    return name;
+  }
+  public in getWeight() {
+    return weight;
+  }
+  public String toString() {
+    String output = "";
+    output += this.getName() + "," + "weights: " + this.getWeight();
+    return output;
 }
