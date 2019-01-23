76509ec3791d037bda43b1f8cb83385ef1688ceb
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2019-01-22 21:55:49 -0500

    Update HorseBarn.java

--- src/com/company/HorseBarn.java
+++ src/com/company/HorseBarn.java
@@ -6,6 +6,19 @@ public class HorseBarn {
         this.spaces = spaces;
     }
     
+    public String toString(){
+        String list = "[";
+        for (int i = 0; i < this.spaces.length;i++){
+            if (this.spaces[i] != null){
+                list += this.spaces[i].getName() + ", ";
+            }
+            else {
+                list += "Empty, ";
+            }
+        }
+        return list.substring(0,list.length()-2)+"]";
+    }
+    
     public int findHorseSpace(String name) {
         for (int i = 0; i < this.spaces.length; i++) {
             if (this.spaces[i]!=null && name.equals(this.spaces[i].getName())) {
